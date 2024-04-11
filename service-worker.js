84e37d30c47693cbeb1fe41dbf058f5c819952de
heijsilva <profissionaljsilva@gmail.com>
2024-04-11 11:35:01 -0300

    Projeto Base

---
+++ service-worker.js
@@ -0,0 +1,43 @@
+const SafeZone = 'v1';
+const ativosEstaticos = [
+  '/',
+  '/index.html', 
+  '/css/style.css',
+  '/script/app.js'
+];
+
+self.addEventListener('install', async evento => {
+  const cache = await caches.open(SafeZone);
+  await cache.addAll(ativosEstaticos);
+});
+
+self.addEventListener('fetch', async evento => {
+  const requisicao = evento.request; 
+  const url = new URL(requisicao.url);
+
+  if (url.origin === location.origin) {
+    evento.respondWith(cachePrimeiro(requisicao));
+  } else {  
+    evento.respondWith(redeECache(requisicao));
+  }
+});
+
+async function cachePrimeiro(requisicao) {
+  const cached = await caches.match(requisicao);
+  return cached || fetch(requisicao);
+}
+
+async function redeECache(requisicao) {
+  const cache = await caches.open(stealdrink);
+
+  try {
+    const fresco = await fetch(requisicao);
+    await cache.put(requisicao, fresco.clone());
+    return fresco;
+
+  } catch (e) {
+    const cached = await cache.match(requisicao);  
+    return cached;
+  }
+
+}
