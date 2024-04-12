ca08e3f05530d10246b9297e0c713a203463dc93
Wectornanime <Wectornanime@gmail.com>
2024-04-12 11:35:16 -0300

    clear code

--- js/home.js
+++ js/home.js
@@ -52,48 +52,4 @@ fetch('../data.json')
 
             mainElement.appendChild(warnElement);
         });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        // response.warns.forEach(warn => {
-        //     mainElement.innerHTML += `
-        //     <section>
-        //     <div class="title">
-        //         <h2>${warn.name}</h2>
-        //     </div>
-        //     <div class="image">
-        //         <div class="content">
-        //             <div class="img-wrapper">
-        //                 <img src="assets/images/${warn.image}" alt="${warn.imageDescription}">
-        //             </div>
-        //             <div class="bg-color"></div>
-        //         </div>
-        //     </div>
-        //     <div class="actions">
-        //         <button><ion-icon name="alert-circle-sharp"></ion-icon></button>
-        //         <button><ion-icon name="help-circle-sharp"></ion-icon></button>
-        //     </div>
-        //     <div class="notification">
-        //         <ion-icon name="warning-outline"></ion-icon>
-        //     </div>
-        // </section>
-        //     `;
-        // });
     });
